2ef701210c18a4647c0ff08148cbe701f2c6c426
Giansar Pratiknya <gpratiknya@gmail.com>
2021-01-27 14:07:46 +0700

    first commit

---
+++ src/main/java/id/giansar/resource/MovieResource.java
@@ -0,0 +1,26 @@
+package id.giansar.resource;
+
+import id.giansar.message.MovieProducer;
+import id.giansar.model.Movie;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MovieResource {
+    @Inject
+    MovieProducer producer;
+
+    @POST
+    public Response send(Movie movie) {
+        producer.sendMovieToKafka(movie);
+        return Response.accepted().build();
+    }
+}
